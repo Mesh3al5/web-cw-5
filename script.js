32f141c1cef0f5693396985749fe9e1be21023dc
Mesh3al5 <100314405+Mesh3al5@users.noreply.github.com>
2022-03-16 20:37:00 +0300

    c5-- all cw

---
+++ script.js
@@ -0,0 +1,36 @@
+alert(`مشعل عادل`)
+
+console.log(14)
+
+console.log(`أنا في الصف التاسع`)
+//comment: جافاسكربت ليست جافا
+
+
+
+
+    let  budget = 20;
+
+
+    console.log(budget > 30 &&  budget < 80);
+
+    budget += 30;
+
+    console.log(budget > 30 &&  budget < 80);
+
+
+
+    let grade = prompt(`اكتب تقديرك`)
+    if (grade >= 90) {
+        console.log(`لقد حصلت على امتياز 🥳`)
+    } else if (grade >= 80) {
+        console.log(`لقد حصلت على جيد جداً🤩`)
+    }else if (grade >= 70) {
+        console.log(`لقد حصلت على جيد🙂`)
+    }else if (grade >= 60) {
+        console.log(`لقد حصلت على مقبول😕`)
+    }else if (grade >= 50) {
+        console.log(`لقد حصلت على ضعيف☹️`)
+    } else {
+        console.log(`راسب💔`)
+    }
+    
